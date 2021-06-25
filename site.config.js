3cb9fd4350030d1f7eeb02d109a480e080be9242
Caaaa-a <59967843+Caaaa-a@users.noreply.github.com>
2021-06-25 14:08:43 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Personal-Website-501a848b13b243e787a79ba2dfac9c3b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Caaaaa Personal Website',
+  domain: 'https://www.notion.so/Personal-Website-501a848b13b243e787a79ba2dfac9c3b',
+  author: 'Caaaaa',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,7 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
